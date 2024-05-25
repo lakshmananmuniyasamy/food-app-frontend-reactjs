19049f1b6ac1446ed723d85bb9a93cfe523ccc68
Lakshmanan <iamlakshmananeee@gmail.com>
2024-05-25 17:54:37 +0530

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your fovorite food here</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingrediants your cavings and elevate your dinnig experience, one delicious meal at a time.</p>
+            <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
